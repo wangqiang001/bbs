850acaddfe22e498d05cf26e001195380ade2a4f
wangqiang001 <2194544736@qq.com>
2019-01-21 21:19:34 +0800

    完成了帖子创建和阅读功能

--- post/views.py
+++ post/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
+from post.models import Post
 # Create your views here.
+
+
+def create_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        post = Post.objects.create(title=title, content=content)
+        return redirect("/post/read/?post_id=%d" % post.id)
+    else:
+        return render(request, 'create_post.html', {})
+
+
+def edit_post(request):
+    return render(request, "edit_post.html", {})
+
+
+def read_post(request):
+    post_id = int(request.GET.get("post_id"))
+    post = Post.objects.get(id=post_id)
+    return render(request, "read_post.html", {"post" : post})
+
+def delete_post(request):
+    return render(request, "delete_post.html", {})
+
+
+def search(request):
+    return render(request, "search.html", {})
+
+
+
+
